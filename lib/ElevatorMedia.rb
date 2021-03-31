7a5357d199f832aedf4a23f951b9f336752f6292
ayadisirine <sirine.ayadi93@gmail.com>
2021-03-31 14:25:37 -0500

    adding code following TDD

--- lib/ElevatorMedia.rb
+++ lib/ElevatorMedia.rb
@@ -57,9 +57,25 @@ module ElevatorMedia
 
       #Get joke by type 
       def GetJokeByType(joketype)
+         # connect to the JOKE API 
+         url = URI('https://jokeapi-v2.p.rapidapi.com/joke/'+joketype)
+      
+         http = Net::HTTP.new(url.host, url.port)
+         http.use_ssl = true
+         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      
+         request = Net::HTTP::Get.new(url)
+         request["x-rapidapi-host"] = 'jokeapi-v2.p.rapidapi.com' 
+         request["x-rapidapi-key"] = '6812dedfadmshd368a8943765d80p1ddf70jsnbb2d18b06af6' 
 
-
-        return nil;
+         response = http.request(request)
+         json_response = JSON.parse(response.body)
+         puts json_response
+         
+         if response.code != "200" || json_response['results'] == 0
+            return "Bad Request"
+         end
+         return json_response
      end
 
 
