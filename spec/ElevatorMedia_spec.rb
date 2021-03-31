0cddfea5ac0a4b7d333fbfc3bf69d0f62f7ac344
ayadisirine <sirine.ayadi93@gmail.com>
2021-03-31 08:36:04 -0500

    another test

--- spec/ElevatorMedia_spec.rb
+++ spec/ElevatorMedia_spec.rb
@@ -1,7 +1,6 @@
 require "ElevatorMedia"
 
-
- 
+#block to group all your tests together & to tell RSpec which class you are testing.
 RSpec.describe ElevatorMedia::Streamer do 
 
    describe ".getContent" do
@@ -12,14 +11,19 @@ RSpec.describe ElevatorMedia::Streamer do
          end
       end
       context 'put a STUB on APIConnect method ' do 
-         it 'returns a predefined Hash' do
+         it 'returns a joke' do
             streamer = ElevatorMedia::Streamer.new
 
             streamer.getContent()
             container = streamer.instance_variable_get('@jokes')
             expect(container['setup']).to be_kind_of String
          end
-      end   
+      end 
+      context 'output type' do 
+        it 'return another joke ' do
+           expect(streamer.getContent()).to be_kind_of String
+        end
+     end  
    end
    
 end
